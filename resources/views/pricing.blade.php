27bff3a2f647fc82996d2f099adbdcf7f2b93244
Tossy Santoso <tossysantos2@gmail.com>
2020-04-17 10:24:23 +0700

    cobasaja

---
+++ resources/views/pricing.blade.php
@@ -0,0 +1,14 @@
+@extends('layout/main-light')
+
+@section('title', 'index')
+
+@section('container')
+<div class="container mt-4">
+<div class="row">
+<div class="col-10">
+<h1 class="mt-10">Laravel - Pricing page</h1>
+{{ $nama_halaman }}
+</div>
+</div>
+</div>
+@endsection
